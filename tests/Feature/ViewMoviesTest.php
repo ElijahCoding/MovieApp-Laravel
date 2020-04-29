45bb5d907e73d1e8e83c359890b0b9f3f73a32b4
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-29 19:48:00 +0300

    testing api

--- tests/Feature/ViewMoviesTest.php
+++ tests/Feature/ViewMoviesTest.php
@@ -3,16 +3,13 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ViewMoviesTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    /** @test */
+    public function the_main_page_shows_correct_info()
     {
         $response = $this->get('/');
 
