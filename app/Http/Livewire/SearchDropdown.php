1a5c65b0db8b8acc4d5487401c719617064e130a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-30 12:26:14 +0300

    search template built

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class SearchDropdown extends Component
 {
+    public $search = 'default';
+
     public function render()
     {
         return view('livewire.search-dropdown');
